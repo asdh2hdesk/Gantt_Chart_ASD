dcf8f2526909e0294721cfdcd1a1e019b25bb85f
URK21CS3015Megha <109952064+URK21CS3015Megha@users.noreply.github.com>
2025-07-29 18:09:05 +0530

    import gantt_task, project_wizard_model in __init__.py

--- gantt_chart/__manifest__.py
+++ gantt_chart/__manifest__.py
@@ -13,18 +13,29 @@
     """,
     'author': 'Rakesh ASD',
     'website': 'https://asdsoftwares.com',
-    'depends': ['base', 'web'],
+    'depends': ['base', 'web', 'project', 'mail'],
     'data': [
         'security/ir.model.access.csv',
         'views/gantt_task_views.xml',
         'views/gantt_menu.xml',
+        'views/project_wizard_view.xml'
+
     ],
     'assets': {
         'web.assets_backend': [
             'gantt_chart/static/src/lib/frappe-gantt.css',
             'gantt_chart/static/src/lib/frappe-gantt.min.js',
             'gantt_chart/static/src/js/frappe_gantt_widget.js',
+            'gantt_chart/static/src/js/combined_gantt_widget.js',
+            'gantt_chart/static/src/css/combined_gantt.css',
             'gantt_chart/static/src/xml/gantt_templates.xml',
+            'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.3.0/exceljs.min.js',
+
         ],
     },
     'installable': True,
--- gantt_chart/models/__init__.py
+++ gantt_chart/models/__init__.py
@@ -1,1 +1,2 @@
 from . import gantt_task
+from. import project_wizard_model
